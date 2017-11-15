97e00593a3a81ba6725ff1bdf12ae816bd035466
Joel Clair <clair.joel@yahoo.com>
2017-11-15 10:42:10 -0800

    paths added for local OR deployed

--- scripts/routes.js
+++ scripts/routes.js
@@ -1,9 +1,13 @@
 if (window.location.pathname !== '/') {
+  console.log('gh paged detected')
   page.base('/knoweathr-client')
+  page('/knoweathr-client/', ctx => app.mapView.initIndexPage(ctx));
+  page('/knoweathr-client/about', ctx => app.mapView.initAboutPage(ctx));
 }
 
-
-page('/', ctx => app.mapView.initIndexPage(ctx));
-page('/about', ctx => app.mapView.initAboutPage(ctx));
+if(window.location.pathname !== '/knoweathr-client') {
+  page('/', ctx => app.mapView.initIndexPage(ctx));
+  page('/about', ctx => app.mapView.initAboutPage(ctx));
+}
 
 page();
