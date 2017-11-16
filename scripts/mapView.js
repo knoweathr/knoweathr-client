bbab6f7d77c5e8be6037367512088a9d1daf8b25
strawbee <yuejiao.hou@gmail.com>
2017-11-16 09:29:44 -0800

    git says I made changes

--- scripts/mapView.js
+++ scripts/mapView.js
@@ -64,251 +64,30 @@ var app = app || {};
       // setTimeout(function() {
         let position = new google.maps.LatLng(parseFloat(airport.lat), parseFloat(airport.lon)); //eslint-disable-line
 
-      //here begins the attempt to switch into whatever month's weather is associated with the airport object
       let pinStyle = '';
       let cond = Object.values(airport)[9];
+      let iconBase = 'https://knoweathr.github.io/knoweathr-client/assets/icons/';
 
       switch(cond) {
       case 'mostly sunny':
-        pinStyle = 'https://knoweathr.github.io/knoweathr-client/assets/icons/sunny.jpg';
+        pinStyle = iconBase + 'sunny.jpg';
         break;
       case 'party cloudy':
-        pinStyle = 'https://knoweathr.github.io/knoweathr-client/assets/icons/pCloudy.jpg';
+        pinStyle = iconBase + 'pCloudy.jpg';
         break;
       case 'rain':
-        pinStyle = 'https://knoweathr.github.io/knoweathr-client/assets/icons/rainy.jpg';
+        pinStyle = iconBase + 'rainy.jpg';
         break;
       case 'mostly cloudy':
-        pinStyle = 'https://knoweathr.github.io/knoweathr-client/assets/icons/cloudy.jpg';
+        pinStyle = iconBase + 'cloudy.jpg';
         break;
       case 'cloudy':
-        pinStyle = 'https://knoweathr.github.io/knoweathr-client/assets/icons/cloudy.jpg';
+        pinStyle = iconBase + 'cloudy.jpg';
         break;
       case 'sunny':
-        pinStyle = 'https://knoweathr.github.io/knoweathr-client/assets/icons/sunny.jpg';
+        pinStyle = iconBase + 'sunny.jpg';
         break;
       }
-      // switch(airport.feb_cloud_cover_cond) {
-      // case 'mostly sunny':
-      //   pinStyle = 'https://knoweathr.github.io/knoweathr-client/assets/icons/sunny.jpg';
-      //   break;
-      // case 'party cloudy':
-      //   pinStyle = 'https://knoweathr.github.io/knoweathr-client/assets/icons/pCloudy.jpg';
-      //   break;
-      // case 'rain':
-      //   pinStyle = 'https://knoweathr.github.io/knoweathr-client/assets/icons/rainy.jpg';
-      //   break;
-      // case 'mostly cloudy':
-      //   pinStyle = 'https://knoweathr.github.io/knoweathr-client/assets/icons/cloudy.jpg';
-      //   break;
-      // case 'cloudy':
-      //   pinStyle = 'https://knoweathr.github.io/knoweathr-client/assets/icons/cloudy.jpg';
-      //   break;
-      // case 'sunny':
-      //   pinStyle = 'https://knoweathr.github.io/knoweathr-client/assets/icons/sunny.jpg';
-      //   break;
-      // }
-      // switch(airport.mar_cloud_cover_cond) {
-      // case 'mostly sunny':
-      //   pinStyle = 'https://knoweathr.github.io/knoweathr-client/assets/icons/sunny.jpg';
-      //   break;
-      // case 'party cloudy':
-      //   pinStyle = 'https://knoweathr.github.io/knoweathr-client/assets/icons/pCloudy.jpg';
-      //   break;
-      // case 'rain':
-      //   pinStyle = 'https://knoweathr.github.io/knoweathr-client/assets/icons/rainy.jpg';
-      //   break;
-      // case 'mostly cloudy':
-      //   pinStyle = 'https://knoweathr.github.io/knoweathr-client/assets/icons/cloudy.jpg';
-      //   break;
-      // case 'cloudy':
-      //   pinStyle = 'https://knoweathr.github.io/knoweathr-client/assets/icons/cloudy.jpg';
-      //   break;
-      // case 'sunny':
-      //   pinStyle = 'https://knoweathr.github.io/knoweathr-client/assets/icons/sunny.jpg';
-      //   break;
-      // }
-      // switch(airport.apr_cloud_cover_cond) {
-      // case 'mostly sunny':
-      //   pinStyle = 'https://knoweathr.github.io/knoweathr-client/assets/icons/sunny.jpg';
-      //   break;
-      // case 'party cloudy':
-      //   pinStyle = 'https://knoweathr.github.io/knoweathr-client/assets/icons/pCloudy.jpg';
-      //   break;
-      // case 'rain':
-      //   pinStyle = 'https://knoweathr.github.io/knoweathr-client/assets/icons/rainy.jpg';
-      //   break;
-      // case 'mostly cloudy':
-      //   pinStyle = 'https://knoweathr.github.io/knoweathr-client/assets/icons/cloudy.jpg';
-      //   break;
-      // case 'cloudy':
-      //   pinStyle = 'https://knoweathr.github.io/knoweathr-client/assets/icons/cloudy.jpg';
-      //   break;
-      // case 'sunny':
-      //   pinStyle = 'https://knoweathr.github.io/knoweathr-client/assets/icons/sunny.jpg';
-      //   break;
-      // }
-      // switch(airport.may_cloud_cover_cond) {
-      // case 'mostly sunny':
-      //   pinStyle = 'https://knoweathr.github.io/knoweathr-client/assets/icons/sunny.jpg';
-      //   break;
-      // case 'party cloudy':
-      //   pinStyle = 'https://knoweathr.github.io/knoweathr-client/assets/icons/pCloudy.jpg';
-      //   break;
-      // case 'rain':
-      //   pinStyle = 'https://knoweathr.github.io/knoweathr-client/assets/icons/rainy.jpg';
-      //   break;
-      // case 'mostly cloudy':
-      //   pinStyle = 'https://knoweathr.github.io/knoweathr-client/assets/icons/cloudy.jpg';
-      //   break;
-      // case 'cloudy':
-      //   pinStyle = 'https://knoweathr.github.io/knoweathr-client/assets/icons/cloudy.jpg';
-      //   break;
-      // case 'sunny':
-      //   pinStyle = 'https://knoweathr.github.io/knoweathr-client/assets/icons/sunny.jpg';
-      //   break;
-      // }
-      // switch(airport.jun_cloud_cover_cond) {
-      // case 'mostly sunny':
-      //   pinStyle = 'https://knoweathr.github.io/knoweathr-client/assets/icons/sunny.jpg';
-      //   break;
-      // case 'party cloudy':
-      //   pinStyle = 'https://knoweathr.github.io/knoweathr-client/assets/icons/pCloudy.jpg';
-      //   break;
-      // case 'rain':
-      //   pinStyle = 'https://knoweathr.github.io/knoweathr-client/assets/icons/rainy.jpg';
-      //   break;
-      // case 'mostly cloudy':
-      //   pinStyle = 'https://knoweathr.github.io/knoweathr-client/assets/icons/cloudy.jpg';
-      //   break;
-      // case 'cloudy':
-      //   pinStyle = 'https://knoweathr.github.io/knoweathr-client/assets/icons/cloudy.jpg';
-      //   break;
-      // case 'sunny':
-      //   pinStyle = 'https://knoweathr.github.io/knoweathr-client/assets/icons/sunny.jpg';
-      //   break;
-      // }
-      // switch(airport.jul_cloud_cover_cond) {
-      // case 'mostly sunny':
-      //   pinStyle = 'https://knoweathr.github.io/knoweathr-client/assets/icons/sunny.jpg';
-      //   break;
-      // case 'party cloudy':
-      //   pinStyle = 'https://knoweathr.github.io/knoweathr-client/assets/icons/pCloudy.jpg';
-      //   break;
-      // case 'rain':
-      //   pinStyle = 'https://knoweathr.github.io/knoweathr-client/assets/icons/rainy.jpg';
-      //   break;
-      // case 'mostly cloudy':
-      //   pinStyle = 'https://knoweathr.github.io/knoweathr-client/assets/icons/cloudy.jpg';
-      //   break;
-      // case 'cloudy':
-      //   pinStyle = 'https://knoweathr.github.io/knoweathr-client/assets/icons/cloudy.jpg';
-      //   break;
-      // case 'sunny':
-      //   pinStyle = 'https://knoweathr.github.io/knoweathr-client/assets/icons/sunny.jpg';
-      //   break;
-      // }
-      // switch(airport.aug_cloud_cover_cond) {
-      // case 'mostly sunny':
-      //   pinStyle = 'https://knoweathr.github.io/knoweathr-client/assets/icons/sunny.jpg';
-      //   break;
-      // case 'party cloudy':
-      //   pinStyle = 'https://knoweathr.github.io/knoweathr-client/assets/icons/pCloudy.jpg';
-      //   break;
-      // case 'rain':
-      //   pinStyle = 'https://knoweathr.github.io/knoweathr-client/assets/icons/rainy.jpg';
-      //   break;
-      // case 'mostly cloudy':
-      //   pinStyle = 'https://knoweathr.github.io/knoweathr-client/assets/icons/cloudy.jpg';
-      //   break;
-      // case 'cloudy':
-      //   pinStyle = 'https://knoweathr.github.io/knoweathr-client/assets/icons/cloudy.jpg';
-      //   break;
-      // case 'sunny':
-      //   pinStyle = 'https://knoweathr.github.io/knoweathr-client/assets/icons/sunny.jpg';
-      //   break;
-      // }
-      // switch(airport.sep_cloud_cover_cond) {
-      // case 'mostly sunny':
-      //   pinStyle = 'https://knoweathr.github.io/knoweathr-client/assets/icons/sunny.jpg';
-      //   break;
-      // case 'party cloudy':
-      //   pinStyle = 'https://knoweathr.github.io/knoweathr-client/assets/icons/pCloudy.jpg';
-      //   break;
-      // case 'rain':
-      //   pinStyle = 'https://knoweathr.github.io/knoweathr-client/assets/icons/rainy.jpg';
-      //   break;
-      // case 'mostly cloudy':
-      //   pinStyle = 'https://knoweathr.github.io/knoweathr-client/assets/icons/cloudy.jpg';
-      //   break;
-      // case 'cloudy':
-      //   pinStyle = 'https://knoweathr.github.io/knoweathr-client/assets/icons/cloudy.jpg';
-      //   break;
-      // case 'sunny':
-      //   pinStyle = 'https://knoweathr.github.io/knoweathr-client/assets/icons/sunny.jpg';
-      //   break;
-      // }
-      // switch(airport.oct_cloud_cover_cond) {
-      // case 'mostly sunny':
-      //   pinStyle = 'https://knoweathr.github.io/knoweathr-client/assets/icons/sunny.jpg';
-      //   break;
-      // case 'party cloudy':
-      //   pinStyle = 'https://knoweathr.github.io/knoweathr-client/assets/icons/pCloudy.jpg';
-      //   break;
-      // case 'rain':
-      //   pinStyle = 'https://knoweathr.github.io/knoweathr-client/assets/icons/rainy.jpg';
-      //   break;
-      // case 'mostly cloudy':
-      //   pinStyle = 'https://knoweathr.github.io/knoweathr-client/assets/icons/cloudy.jpg';
-      //   break;
-      // case 'cloudy':
-      //   pinStyle = 'https://knoweathr.github.io/knoweathr-client/assets/icons/cloudy.jpg';
-      //   break;
-      // case 'sunny':
-      //   pinStyle = 'https://knoweathr.github.io/knoweathr-client/assets/icons/sunny.jpg';
-      //   break;
-      // }
-      // switch(airport.nov_cloud_cover_cond) {
-      // case 'mostly sunny':
-      //   pinStyle = 'https://knoweathr.github.io/knoweathr-client/assets/icons/sunny.jpg';
-      //   break;
-      // case 'party cloudy':
-      //   pinStyle = 'https://knoweathr.github.io/knoweathr-client/assets/icons/pCloudy.jpg';
-      //   break;
-      // case 'rain':
-      //   pinStyle = 'https://knoweathr.github.io/knoweathr-client/assets/icons/rainy.jpg';
-      //   break;
-      // case 'mostly cloudy':
-      //   pinStyle = 'https://knoweathr.github.io/knoweathr-client/assets/icons/cloudy.jpg';
-      //   break;
-      // case 'cloudy':
-      //   pinStyle = 'https://knoweathr.github.io/knoweathr-client/assets/icons/cloudy.jpg';
-      //   break;
-      // case 'sunny':
-      //   pinStyle = 'https://knoweathr.github.io/knoweathr-client/assets/icons/sunny.jpg';
-      //   break;
-      // }
-      // switch(airport.dec_cloud_cover_cond) {
-      // case 'mostly sunny':
-      //   pinStyle = 'https://knoweathr.github.io/knoweathr-client/assets/icons/sunny.jpg';
-      //   break;
-      // case 'party cloudy':
-      //   pinStyle = 'https://knoweathr.github.io/knoweathr-client/assets/icons/pCloudy.jpg';
-      //   break;
-      // case 'rain':
-      //   pinStyle = 'https://knoweathr.github.io/knoweathr-client/assets/icons/rainy.jpg';
-      //   break;
-      // case 'mostly cloudy':
-      //   pinStyle = 'https://knoweathr.github.io/knoweathr-client/assets/icons/cloudy.jpg';
-      //   break;
-      // case 'cloudy':
-      //   pinStyle = 'https://knoweathr.github.io/knoweathr-client/assets/icons/cloudy.jpg';
-      //   break;
-      // case 'sunny':
-      //   pinStyle = 'https://knoweathr.github.io/knoweathr-client/assets/icons/sunny.jpg';
-      //   break;
-      // }
-      //here ends the aiport weather switch
 
       let marker = new google.maps.Marker({ //eslint-disable-line
         position: position,
@@ -317,11 +96,6 @@ var app = app || {};
         title: airport.name,
         icon: pinStyle
       })
-      // console.log(airport.jul_cloud_cover_cond);
-      // console.log(airport);
-      // }, airport * 1000)
-
-      // console.log(airport[may_cloud_cover_cond]);
     })
   }
 
