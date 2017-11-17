5cbd317aab11cb7d64e71a7b2ca7fcc035893dd6
strawbee <yuejiao.hou@gmail.com>
2017-11-17 09:33:11 -0800

    fixing show more button

--- scripts/mapView.js
+++ scripts/mapView.js
@@ -125,7 +125,12 @@ var app = app || {};
                 })
                   .catch(err => console.error(err));
                 $(`#favmsg${i}`).text('Added to favorites! ');
+                $('#showmore').hide();
                 if (window.innerWidth < 1001) $(`#favmsg${i}`).append('<br />Go to your login page to view your favorites.');
+                if ($('#renderfavorites ul:nth-of-type(3)')) {
+                  $('#showmore').show();
+                  $('#showless').hide();
+                }
               } else {
                 if (data.includes(JSON.stringify(app.weather.filteredInfo[i]))) {
                   $(`#favmsg${i}`).text('You have already added this favorite.');
@@ -141,6 +146,11 @@ var app = app || {};
                     .catch(err => console.error(err));
                   $(`#favmsg${i}`).text('Added to favorites!');
                   if (window.innerWidth < 1001) $(`#favmsg${i}`).append('<br />Go to your login page to view your favorites.');
+                  $('#showmore').hide();
+                  if ($('#renderfavorites ul:nth-of-type(3)')) {
+                    $('#showmore').show();
+                    $('#showless').hide();
+                  }
                 }
               }
             }
